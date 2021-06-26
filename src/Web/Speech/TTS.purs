33332ff4736818bc7f407484c0aa31ecde90a09a
Dirk Zimmermann <me@dirkz.com>
2021-06-26 13:01:06 +0200

    Export Voice

--- src/Web/Speech/TTS.purs
+++ src/Web/Speech/TTS.purs
@@ -1,12 +1,11 @@
 -- |Wrapper around Web Speech Text-to-speech (TTS)
-module Web.Speech.TTS (getVoices) where
-
-import Prelude
+module Web.Speech.TTS (getVoices, Voice) where
 
 import Effect.Aff (Aff)
 import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 
-type Voice = {lang :: String, name :: String}
+type Voice
+  = { lang :: String, name :: String }
 
 foreign import _getVoices :: EffectFnAff (Array Voice)
 
