cac02001cab792ef4aaccc59e96e3011f8fc9064
Dirk Zimmermann <me@dirkz.com>
2021-06-26 11:45:23 +0200

    Log if we're even called

--- src/Web/Speech/TTS.js
+++ src/Web/Speech/TTS.js
@@ -1,7 +1,8 @@
 "strict";
 
-exports._getVoices = function(onError, onSuccess) {
-    let innerGetVoices = function() {
+exports._getVoices = function (onError, onSuccess) {
+    console.log("*** _getVoices")
+    let innerGetVoices = function () {
         let voices = synth.getVoices();
         console.log("*** have voices")
         onSuccess(voices);
