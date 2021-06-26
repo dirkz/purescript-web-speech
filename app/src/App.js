db7fa1fba3662a7ef0cf0ce162db59d70dcb88b5
Dirk Zimmermann <me@dirkz.com>
2021-06-26 10:08:54 +0200

    Remove unused cruft

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { jsCounter as Counter } from "./output/App.Components.Counter/";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <Counter label="Click me!" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
