ce7e423a130d3f6695fdc917fec49bc043d88eed
Dirk Zimmermann <me@dirkz.com>
2021-06-26 12:25:32 +0200

    Remove js console log

--- src/Web/Speech/TTS.js
+++ src/Web/Speech/TTS.js
@@ -1,22 +1,18 @@
 "strict";
 
 exports._getVoices = function (onError, onSuccess) {
-    console.log("*** _getVoices")
     let innerGetVoices = function () {
         let voices = synth.getVoices();
-        console.log("*** have voices")
         onSuccess(voices);
     }
     let synth = window.speechSynthesis;
     if (speechSynthesis.onvoiceschanged !== undefined) {
-        console.log("*** have onvoiceschanged")
         speechSynthesis.onvoiceschanged = innerGetVoices;
         let voices = synth.getVoices();
         if (voices.length > 0) {
             onError(voices);
         }
     } else {
-        console.log("*** can call directly")
         innerGetVoices();
     }
 }
