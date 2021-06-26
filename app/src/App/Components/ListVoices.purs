8a94d83efdcac018cf11017aff524f9dd1aa0e2e
Dirk Zimmermann <me@dirkz.com>
2021-06-26 11:12:09 +0200

    Try to show the tts voices

--- app/src/App/Components/ListVoices.purs
+++ app/src/App/Components/ListVoices.purs
@@ -1,13 +1,18 @@
 module App.Components.ListVoices (jsListVoices) where
 
 import Prelude
+import Control.Monad.Error.Class (try)
+import Data.Either (Either(..))
 import Data.Interpolate (i)
 import Data.Tuple.Nested ((/\))
+import Effect.Aff (runAff)
+import Effect.Class (liftEffect)
 import Effect.Unsafe (unsafePerformEffect)
-import React.Basic.DOM (button, div_, p_, text)
+import React.Basic.DOM (button, div_, li_, p_, text, ul_)
 import React.Basic.Events (handler_)
 import React.Basic.Hooks (Component, JSX, component, useState')
 import React.Basic.Hooks as Hooks
+import Web.Speech.TTS as TTS
 
 type Props
   = {}
@@ -18,12 +23,19 @@ jsListVoices = unsafePerformEffect mkListVoices
 mkListVoices :: Component Props
 mkListVoices =
   component "Counter" \props -> Hooks.do
-    count /\ setCount <- useState' 0
+    voices /\ setVoices <- useState' []
+    Hooks.useEffectOnce do
+      runAff do
+        eitherVoices <- try TTS.getVoices
+        case eitherVoices of
+          Left voices -> setVoices voices -- TODO: Show the error
+          Right voices -> setVoices voices
     pure do
       div_
-        [ p_ [ text $ i "You clicked " count " times" ]
-        , button
-            { onClick: handler_ $ setCount (count + 1)
-            , children: [ text "Click me" ]
-            }
-        ]
+        [ ul_ (map listItem voices) ]
+  where
+  listItem voice =
+    let
+      string = voice.name
+    in
+      li_ string
