b7c4c6424752e667e4560d7ae89201640024c280
Dirk Zimmermann <me@dirkz.com>
2021-06-26 12:34:44 +0200

    Use the object from the callback

--- src/Web/Speech/TTS.js
+++ src/Web/Speech/TTS.js
@@ -1,11 +1,13 @@
 "strict";
 
 exports._getVoices = function (onError, onSuccess) {
-    let innerGetVoices = function () {
-        let voices = synth.getVoices();
+    let innerGetVoices = function (theSynth, ev) {
+        let voices = theSynth.getVoices();
         onSuccess(voices);
     }
+
     let synth = window.speechSynthesis;
+
     if (speechSynthesis.onvoiceschanged !== undefined) {
         speechSynthesis.onvoiceschanged = innerGetVoices;
         synth.getVoices(); // trigger a voice fetch
