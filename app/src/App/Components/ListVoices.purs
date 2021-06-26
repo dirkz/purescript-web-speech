ebd9998fbe28595396ea2f5830efc90917c7a7dd
Dirk Zimmermann <me@dirkz.com>
2021-06-26 14:59:36 +0200

    Slightly nicer layout

--- app/src/App/Components/ListVoices.purs
+++ app/src/App/Components/ListVoices.purs
@@ -61,12 +61,15 @@ mkListVoices =
 
       theButton = elem button { onClick: handler_ speakIt } [ text sayItText ]
     in
-      elem li { title: string } [ elem D.div {} [ text string, theButton ] ]
+      elemClass D.div "row" { title: string }
+        [ elemClass D.div "col-1" {} [ text string ]
+        , elemClass D.div "col-2" {} [ theButton ]
+        ]
 
   content state int = case state of
     VoiceStateInitial -> text "Waiting for voices ..."
     VoiceStateError s -> text $ "Error: " <> s
-    VoiceStateVoices vs -> ul_ (map (listItem int) vs)
+    VoiceStateVoices vs -> elemClass D.div "container" {} (map (listItem int) vs)
 
   nextRandomInt setter = do
     anInt <- randomInt 1 999
