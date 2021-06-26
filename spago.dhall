2aee8a7038b52a95fa7849e8b75264d5a6b8dbfb
Dirk Zimmermann <me@dirkz.com>
2021-06-26 10:56:36 +0200

    Weird type, but it compiles

--- spago.dhall
+++ spago.dhall
@@ -11,7 +11,8 @@ When creating a new Spago project, you can use
 to generate this file without the comments in this block.
 -}
 { name = "web-speech"
-, dependencies = [ "aff", "console", "effect", "prelude", "psci-support" ]
+, dependencies =
+  [ "aff", "console", "effect", "prelude", "psci-support", "transformers" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Web/Speech/TTS.purs
+++ src/Web/Speech/TTS.purs
@@ -1,8 +1,15 @@
 -- |Wrapper around Web Speech Text-to-speech (TTS)
-module Web.Speech.TTS where
+module Web.Speech.TTS (getVoices) where
 
-import Effect.Aff.Compat (EffectFnAff)
+import Prelude
+
+import Control.Monad.List.Trans (fromEffect)
+import Effect.Aff (Aff)
+import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 
 type Voice = {lang :: String, name :: String}
 
-foreign import _getVoices :: EffectFnAff (Array Voice)
+foreign import _getVoices :: Unit -> EffectFnAff (Array Voice)
+
+getVoices :: Unit -> Aff (Array Voice)
+getVoices = fromEffectFnAff <<< _getVoices
