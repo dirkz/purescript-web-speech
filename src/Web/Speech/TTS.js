9447da96826a645e2ed9cf2d35338de428abb1f2
Dirk Zimmermann <me@dirkz.com>
2021-06-26 12:49:37 +0200

    Better state

--- src/Web/Speech/TTS.js
+++ src/Web/Speech/TTS.js
@@ -1,17 +1,28 @@
 "strict";
 
 exports._getVoices = function (onError, onSuccess) {
-    let innerGetVoices = function (theSynth, ev) {
-        let voices = theSynth.getVoices();
-        onSuccess(voices);
-    }
-
+    var numberOfAttemptsWithZeroVoices = 0
     let synth = window.speechSynthesis;
+
+    let innerGetVoices = function () {
+        let voices = synth.getVoices();
+
+        if (voices.length == 0) {
+            if (numberOfAttemptsWithZeroVoices > 0) {
+                onError("No voices found")
+            } else {
+                numberOfAttemptsWithZeroVoices++
+            }
+        }
+
+        if (voices.length != 0) {
+            onSuccess(voices);
+        }
+    }
 
     if (speechSynthesis.onvoiceschanged !== undefined) {
         speechSynthesis.onvoiceschanged = innerGetVoices;
-        synth.getVoices(); // trigger a voice fetch
-    } else {
-        innerGetVoices();
     }
+
+    innerGetVoices();
 }
