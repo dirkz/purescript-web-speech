2f223607c0c4e1d5a2786238f50873e618726bef
Dirk Zimmermann <me@dirkz.com>
2021-06-26 11:34:56 +0200

    It finally compiles

--- app/src/App/Components/ListVoices.purs
+++ app/src/App/Components/ListVoices.purs
@@ -1,15 +1,11 @@
 module App.Components.ListVoices (jsListVoices) where
 
 import Prelude
-import Control.Monad.Error.Class (try)
 import Data.Either (Either(..))
-import Data.Interpolate (i)
 import Data.Tuple.Nested ((/\))
-import Effect.Aff (runAff)
-import Effect.Class (liftEffect)
+import Effect.Aff (runAff_)
 import Effect.Unsafe (unsafePerformEffect)
-import React.Basic.DOM (button, div_, li_, p_, text, ul_)
-import React.Basic.Events (handler_)
+import React.Basic.DOM (div_, li, ul_)
 import React.Basic.Hooks (Component, JSX, component, useState', useEffectOnce)
 import React.Basic.Hooks as Hooks
 import Web.Speech.TTS as TTS
@@ -24,18 +20,21 @@ mkListVoices :: Component Props
 mkListVoices =
   component "Counter" \_ -> Hooks.do
     voices /\ setVoices <- useState' []
-    useEffectOnce do
-      runAff do
-        eitherVoices <- try TTS.getVoices
-        case eitherVoices of
-          Left theVoices -> liftEffect $ setVoices theVoices -- TODO: Show the error
-          Right theVoices -> liftEffect $ setVoices theVoices
+    useEffectOnce
+      $ pure
+      $ runAff_
+          (receivedVoices setVoices)
+          TTS.getVoices
     pure do
       div_
         [ ul_ (map listItem voices) ]
   where
+  receivedVoices setter eitherVoices = case eitherVoices of
+    Left _ -> setter [] -- TODO: Show the error
+    Right theVoices -> setter theVoices
+
   listItem voice =
     let
       string = voice.name
     in
-      li_ string
+      li { title: string }
