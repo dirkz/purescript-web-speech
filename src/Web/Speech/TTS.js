44f017ba0b6dae7fd557cea97767c2a14195c415
Dirk Zimmermann <me@dirkz.com>
2021-06-26 11:41:12 +0200

    Some console.log

--- src/Web/Speech/TTS.js
+++ src/Web/Speech/TTS.js
@@ -3,16 +3,19 @@
 exports._getVoices = function(onError, onSuccess) {
     let innerGetVoices = function() {
         let voices = synth.getVoices();
+        console.log("*** have voices")
         onSuccess(voices);
     }
     let synth = window.speechSynthesis;
     if (speechSynthesis.onvoiceschanged !== undefined) {
+        console.log("*** have onvoiceschanged")
         speechSynthesis.onvoiceschanged = innerGetVoices;
         let voices = synth.getVoices();
         if (voices.length > 0) {
             onError(voices);
         }
     } else {
+        console.log("*** can call directly")
         innerGetVoices();
     }
 }
