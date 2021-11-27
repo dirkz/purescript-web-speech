8a13b701d0f16caf0327406c150b4f4a09d0dc0a
Dirk Zimmermann <me@dirkz.com>
2021-11-27 09:45:59 +0100

    More docs

--- web-speech/src/Web/Speech/Synthesis/Voice.purs
+++ web-speech/src/Web/Speech/Synthesis/Voice.purs
@@ -1,3 +1,5 @@
+--| The SpeechSynthesisVoice interface of the Web Speech API represents a voice that the system supports. Every SpeechSynthesisVoice has its own relative speech service including information about language, name and URI.
+--|
 --| See https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice
 module Web.Speech.Synthesis.Voice where
 
