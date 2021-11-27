6a98b858e643414ddfb9c182d6e2cadc57e28e41
Dirk Zimmermann <me@dirkz.com>
2021-11-27 11:40:08 +0100

    Export the voice first

--- web-speech/src/Web/Speech/Synthesis/Voice.purs
+++ web-speech/src/Web/Speech/Synthesis/Voice.purs
@@ -1,7 +1,7 @@
 --|The SpeechSynthesisVoice interface of the Web Speech API represents a voice that the system supports. Every SpeechSynthesisVoice has its own relative speech service including information about language, name and URI.
 --|
 --|[MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice)
-module Web.Speech.Synthesis.Voice (default, lang, Voice) where
+module Web.Speech.Synthesis.Voice (Voice, default, lang) where
 
 import Data.Function.Uncurried (Fn1, runFn1)
 
