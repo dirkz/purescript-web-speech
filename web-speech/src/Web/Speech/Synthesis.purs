e4ec18dc3b53f8c3440c82725849a1613b880f96
Dirk Zimmermann <me@dirkz.com>
2021-11-27 16:17:39 +0100

    Use Nullable with Maybe

--- web-speech/src/Web/Speech/Synthesis.purs
+++ web-speech/src/Web/Speech/Synthesis.purs
@@ -8,21 +8,23 @@ module Web.Speech.Synthesis
 
 import Prelude
 
+import Data.Nullable (Nullable, toMaybe)
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Aff.Compat (EffectFn1, EffectFnAff, fromEffectFnAff, runEffectFn1)
 import Web.HTML.Window (Window)
 import Web.Speech.Synthesis.Voice (Voice)
+import Data.Maybe (Maybe)
 
 --|Represents a [SpeechSynthesis](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis)
 foreign import data Synthesis :: Type
 
-foreign import _synthesis :: EffectFn1 Window Synthesis
+foreign import _synthesis :: EffectFn1 Window (Nullable Synthesis)
 
 --|The global `speechSynthesis` object from the given `Window`.
 --|[MDN.](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis)
-synthesis :: Window -> Effect Synthesis
-synthesis = runEffectFn1 _synthesis
+synthesis :: Window -> Effect (Maybe Synthesis)
+synthesis = map toMaybe <<< runEffectFn1 _synthesis
 
 foreign import _voices :: Synthesis -> EffectFnAff (Array Voice)
 
