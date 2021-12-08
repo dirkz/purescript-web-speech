43e20295f1f5076da264def85d17cd5444c58b16
Dirk Zimmermann <me@dirkz.com>
2021-12-08 13:38:04 +0100

    Start an utterance

---
+++ src/Web/Speech/TTS/Utterance.purs
@@ -0,0 +1,4 @@
+module Web.Speech.TTS.Utterance where
+
+--|Represents a [SpeechSynthesisUtterance](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance)
+foreign import data Utterance :: Type
