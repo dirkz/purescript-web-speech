23e1e87e49f302bebbfb803ac8c578d78345e896
Dirk Zimmermann <me@dirkz.com>
2021-12-08 09:53:00 +0100

    Unset the listener

--- src/Web/Speech/TTS.js
+++ src/Web/Speech/TTS.js
@@ -8,16 +8,24 @@ exports._voices = function (synth) {
     return function (onError, onSuccess) {
         var numberOfAttemptsWithZeroVoices = 0;
 
+        let unsetVoiceListener = function () {
+            if (speechSynthesis.onvoiceschanged !== undefined) {
+                speechSynthesis.onvoiceschanged = undefined;
+            }
+        };
+
         let innerGetVoices = function () {
             let voices = synth.getVoices();
 
             if (voices.length == 0) {
                 if (numberOfAttemptsWithZeroVoices > 0) {
+                    unsetVoiceListener();
                     onError("No voices found");
                 } else {
                     numberOfAttemptsWithZeroVoices++;
                 }
             } else if (voices.length > 0) {
+                unsetVoiceListener();
                 onSuccess(voices);
             }
         };
