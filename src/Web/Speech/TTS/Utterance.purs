e7b383b186d0f47dcf7b8237143bf0411e4a9ea9
Dirk Zimmermann <me@dirkz.com>
2021-12-08 14:19:37 +0100

    Try to work with "spec" for parameters

--- src/Web/Speech/TTS/Utterance.purs
+++ src/Web/Speech/TTS/Utterance.purs
@@ -1,4 +1,17 @@
-module Web.Speech.TTS.Utterance where
+module Web.Speech.TTS.Utterance
+  ( defaultSpec
+  )
+  where
 
 --|Represents a [SpeechSynthesisUtterance](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance)
 foreign import data Utterance :: Type
+
+--|Variable parameters for an utterance.
+type UtteranceSpec
+  = { pitch :: Number
+    , rate :: Number
+    , volume :: Number
+    }
+
+defaultSpec :: UtteranceSpec
+defaultSpec = { pitch: 1.0, rate: 1.0, volume: 1.0 }
