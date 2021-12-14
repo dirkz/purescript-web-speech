e002513b780c156da4dafa42c8277198a6691c3c
Dirk Zimmermann <me@dirkz.com>
2021-12-14 09:46:43 +0100

    Update the license script with the spago one

--- get_licenses.sh
+++ get_licenses.sh
@@ -3,8 +3,5 @@
 # Note: the `awk` part is to cut out only the package name
 for dep in $(spago ls deps | awk '{print $1}')
 do
-  echo
-  echo ${dep}:
-  echo
-  cat $(find ".spago/${dep}" -iname 'LICENSE_THIRDPARTY')
+    cat $(find ".spago/${dep}" -iname 'LICENSE_THIRDPARTY')
 done
